73ccd71037cec1f397ef444f70ab1d5da2782b3e
annesofiefogh <anne2z08@stud.kea.dk>
2023-05-09 09:05:58 +0200

    DB first draft

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,25 +2,16 @@ import { MongoClient } from "mongodb";
 
 const URL = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(URL);
+
 await client.connect();
+console.log('Connected to MongoDB');
 
-const db = client.db("food_universe");
-
-export default {
-    users: db.collection("users")
-}
-
-
-/*
-const URL = "mongodb://127.0.0.1:27017";
-const client = new MongoClient(URL);
-await client.connect();
-
-const db = client.db("food_universe");
-console.log(db);
+const dbToDelete = client.db("food_universe");
+dbToDelete.dropDatabase();
+const db = client.db("food_universe");      
 
 
 export default {
+    db,
     users: db.collection("users")
 }
-*/
--- server/database/createDatabase.js
+++ server/database/createDatabase.js
@@ -1,20 +1,19 @@
-import db  from "./connection.js";
+import db from "./connection.js";
 import bcrypt from "bcrypt";
 
-const isDeleteMode = process.argv.findIndex((argument) => argument === "delete_mode") === -1 ? false : true;
+const isDeleteMode = true;
 
 if (isDeleteMode) {
-    console.log('farvel');
-    db.users.drop(); 
-} 
+    const adminPlainPassword = "admin123";
+    const adminHashedPassword = await bcrypt.hash(adminPlainPassword, 12);
 
-const adminPlainPassword = "admin123";
-const adminHashedPassword = await bcrypt.hash(adminPlainPassword, 12);
+    const userPlainPassword = "user123";
+        const userHashedPassword = await bcrypt.hash(userPlainPassword, 12);
 
-const userPlainPassword = "user123";
-const userHashedPassword = await bcrypt.hash(userPlainPassword, 12);
+    db.users.insertMany([
+        { username: "admin", email: "admin@mail.dk", password: adminHashedPassword, role_id: 1 }, 
+        { username: "user", email: "user@mail.dk", password: userHashedPassword, role_id: 2 }
+    ]);
 
-db.users.insertMany([
-    { username: "admin", email: "admin@mail.dk", password: adminHashedPassword, role_id: 1 }, 
-    { username: "user", email: "user@mail.dk", password: userHashedPassword, role_id: 2 }
-]);
+}
+
