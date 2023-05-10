5e233b70fce9b8517aaf2edc68c0b479aa32645c
LauraStorm <lbs2rm@hotmail.com>
2023-05-10 13:03:17 +0200

    added validator to sanitize in authRouter.js

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -3,16 +3,19 @@ const router = Router();
 
 import db from "../database/connectionAtlas.js"
 import bcrypt from "bcrypt";
+import validator from "validator";
 
 // login
 router.post("/api/auth/login", async (req, res) => {
     const { password, email } = req.body;
+    const sanitizedPassword = validator.escape(password);
+    const sanitizedEmail = validator.escape(email);
     console.log("req.body: ", req.body);
     
-    const userExist = await db.collection("users").find({ email: email }).toArray();
+    const userExist = await db.collection("users").find({ email: sanitizedEmail }).toArray();
     console.log("User exist: ", userExist);
 
-    if (userExist.length === 0 || !(await bcrypt.compare(password, userExist[0].password))) {
+    if (userExist.length === 0 || !(await bcrypt.compare(sanitizedPassword, userExist[0].password))) {
         // log in
         console.log("invalid user : ", userExist);
 
@@ -40,26 +43,28 @@ router.get("/api/auth/logout", (req, res) => {
 // signup
 router.post("/api/auth/signup", async (req, res) => {
     const { username, password, email } = req.body;
+    const sanitizedUsername = validator.escape(username);
+    const sanitizedPassword = validator.escape(password);
+    const sanitizedEmail = validator.escape(email);
     console.log("2 ",req.body);
     
-    const userExist = await db.collection("users").find({ email: email }).toArray();
+    const userExist = await db.collection("users").find({ email: sanitizedEmail }).toArray();
     console.log("3 ", userExist);
 
     if (userExist.length === 0) {
-        const hashedPassword = await bcrypt.hash(password, 12);
+        const hashedPassword = await bcrypt.hash(sanitizedPassword, 12);
         const userRole = 2;
 
-        const newUser = { username: username, email: email, password: hashedPassword, role_id: userRole };
+        const newUser = { username: sanitizedUsername, email: sanitizedEmail, password: hashedPassword, role_id: userRole };
         console.log("4 ",newUser);
 
         db.collection("users").insertOne(newUser);
 
-        res.status(200).send({ data: username });
+        res.status(200).send({ data: sanitizedUsername });
     } else {
         console.log("Signup failed");
         res.status(409).send({ message: "error" });
     }
-
 });
 
 export default router;
