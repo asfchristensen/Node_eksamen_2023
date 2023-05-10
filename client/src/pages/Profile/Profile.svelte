337b8fc1dbb2cbeaffa74161581e8a8ec7f6a06b
annesofiefogh <anne2z08@stud.kea.dk>
2023-05-10 13:30:21 +0200

    A little frontend

--- client/src/pages/Profile/Profile.svelte
+++ client/src/pages/Profile/Profile.svelte
@@ -1,12 +1,32 @@
 <script>
+    import { onMount } from "svelte";
+    import { BASE_URL } from "../../stores/urlDomain.js";
     import { user } from "../../stores/user.js";
 
+
+    onMount(async () => {
+        const recipesURL = null;
+    });
     $: username = $user;
 </script>
 
 <h2>Profile</h2>
 <h5>Welcome to your profile page <span class="user">{username}</span></h5>
 
+
+<!--
+<div class="form-signup">
+    <form on:submit|preventDefault={handleSignup}>
+        <input type="text" placeholder="username" name="username" bind:value={username} required><br><br>
+
+        <input type="password" placeholder="password" name="password" bind:value={password} required><br><br>
+
+        <input type="email" placeholder="email" name="email" bind:value={email} required><br><br>
+
+        <button type="submit">Sign up</button>
+    </form>
+</div>
+-->
 
 <style>
     .user {
